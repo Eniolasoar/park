31c86c13d4d5fb3e9420bdfcc81df90f1b0b99fc
Anishe Eniola Olumide <eniolanishe@gmail.com>
2024-01-14 16:06:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello!');
+const observer=new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("showPage");
+        }else{
+            entry.target.classList.remove("showPage");
+        }
+    })
+});
+
+const observer2=new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("showPage2");
+        }
+        else{
+            entry.target.classList.remove("showPage2")
+        }
+    })
+})
+
+const elements=document.querySelectorAll(".hiddenPage");
+elements.forEach((el)=>observer.observe(el));
+
+// const elements2=document.querySelectorAll(".hiddenPage2");
+// elements2.forEach((el2)=>observer2.observe(el2));
